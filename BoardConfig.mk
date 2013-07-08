2433c1432c5f2cc22ee0eeadb56e6d061b639daa
Felix Obenhuber <felix@obenhuber.de>
2013-07-08 19:27:35 +0200

    added kernel build
    the prebuilt kernel is still available under recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,19 +23,20 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x4B000000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x4CBB4000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/skomer/recovery/recovery_kernel
 TARGET_RECOVERY_INITRC = device/samsung/skomer/recovery/root/init.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skomer/recovery/graphics.c
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_SUPPRESS_EMMC_WIPE := true
 BOARD_HAS_NO_MISC_PARTITION := true
 
-#TARGET_KERNEL_SOURCE := kernel/samsung/skomer
-#TARGET_KERNEL_CONFIG := skomer_defconfig
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/skomer
+TARGET_KERNEL_CONFIG := skomer_defconfig
+TARGET_USE_ST_ERICSSON_KERNEL := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/skomer/recovery/recovery_kernel
+#TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/skomer/recovery/recovery_kernel
 TARGET_RECOVERY_INITRC = device/samsung/skomer/recovery/root/init.rc
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skomer/recovery/graphics.c
 BOARD_HAS_NO_MISC_PARTITION := true
--- device_skomer.mk
+++ device_skomer.mk
@@ -14,9 +14,6 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
-
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
