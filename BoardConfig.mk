9f7238eb634d0214e37b88266b0bb5c72c4eb8e9
Felix Obenhuber <felix@obenhuber.de>
2013-07-27 10:24:36 +0200

    added some C lines
    Change-Id: Ie44402af292a1ef7a06b05cd8f4c9053536e7413

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2013 The Android Open Source Project
 # Copyright (C) 2013 Óliver García Albertos (oliverarafo@gmail.com)
+# Copyright (C) 2013 Felix Obenhuber (f.obenhuber@gmail.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
