bbc18f28e6e165fc9883d081fd40dbff70e6bb64
Felix Obenhuber <felix@obenhuber.de>
2013-09-08 19:54:34 +0200

    bump to u8500 common stuff
    Change-Id: If94bdf8e10bb388fa385faa3a99b4bf51d88b7e1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,32 +14,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/skomer/include
+-include device/samsung/u8500-common/BoardCommonConfig.mk
+
 TARGET_OTA_ASSERT_DEVICE := skomer,s7710,GT-S7710
 
-# Board
 TARGET_BOOTLOADER_BOARD_NAME := DB8520H
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Platform 
-TARGET_BOARD_PLATFORM := montblanc
-BOARD_USES_STE_HARDWARE := true
-TARGET_SOC := u8500
-COMMON_GLOBAL_CFLAGS += -DSTE_HARDWARE -DSTE_SAMSUNG_HARDWARE
-
-# Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_ARCH_VARIANT_CPU := cortex-a9
-TARGET_CPU_SMP := true
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
 # Partitions
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1258291200
@@ -57,48 +37,13 @@ TARGET_USE_ST_ERICSSON_KERNEL := true
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_RECOVERY_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_FORCE_RAMDISK_ADDRESS := 0x02000000
-
-# Graphics
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/skomer/prebuilt/system/lib/egl/egl.cfg
-
-# Screen
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
-
-# Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
+# TODO: check if this is needed
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Wifi
-BOARD_WLAN_DEVICE := bcmdhd
-BOARD_WLAN_DEVICE_REV := bcm4334_B2
-WPA_SUPPLICANT_VERSION := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/dhd.ko"
-WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin_b2"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin_b2"
-WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin_b2"
-WIFI_DRIVER_MODULE_NAME := "dhd"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
-WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
-WIFI_BAND := 802_11_ABG
-
-# Browser
-ENABLE_WEBGL := true
-
-# Audio
-BOARD_USES_GENERIC_AUDIO := false
-BOARD_USES_ALSA_AUDIO := true
-
-# USB Mounting
-BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun0/file"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/musb-ux500.0/musb-hdrc/gadget/lun%d/file"
+#WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
+#WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin_b2 nvram_path=/system/etc/wifi/nvram_net.txt_GPIO4"
+#WIFI_BAND := 802_11_ABG
 
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 25
@@ -106,10 +51,5 @@ BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 
 # Recovery
-TARGET_PROVIDES_INIT_RC := true
-BOARD_HAS_NO_SELECT_BUTTON := true
+#TARGET_PROVIDES_INIT_RC := true
 TARGET_RECOVERY_INITRC = device/samsung/skomer/recovery/recovery.rc
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/skomer/recovery/graphics.c
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_SUPPRESS_EMMC_WIPE := true
-BOARD_HAS_NO_MISC_PARTITION := true
